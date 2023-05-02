a0826fd9e480dbaa46bf60385695c25df728af4b
nekoraw <michelbarros123456@gmail.com>
2023-05-02 20:27:40 -0300

    impementação simples do sistema de login

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from quart import Quart
+from quart import Quart, request, render_template, make_response
 from beanie import init_beanie
 from src.db.HomeTest import HomeTest
+from src.db.Session import Session
+from src.db.User import User
 from src.db.parse_login import create_mongodb_uri
 from motor.motor_asyncio import AsyncIOMotorClient
 import dotenv
 import os
 
-app = Quart("review_professores")
+app = Quart("review_professores", template_folder="src/templates")
+
 
 @app.before_serving
 async def init_database():
@@ -20,11 +23,34 @@ async def init_database():
 
    uri = create_mongodb_uri(mongodb_server, mongodb_port, mongodb_username, mongodb_password, mongodb_auth_db)
    client = AsyncIOMotorClient(uri)
-   await init_beanie(getattr(client, mongodb_database), document_models=[HomeTest])
+   await init_beanie(getattr(client, mongodb_database), document_models=[HomeTest, User, Session])
 
 @app.route("/")
 async def hello_world():
     n_entries = len(await HomeTest.find({}).to_list())
     return f"<p>Hello, World! There are {n_entries} entries in the database.</p>"
+
+@app.route("/login", methods=['GET', 'POST'])
+async def login():
+   if request.method == "POST":
+      username = (await request.form)['username']
+      password_hash = (await request.form)['password_hash']
+      user = await User.find({"username": username}).first_or_none()
+      if user is not None:
+         if user.password_hash == password_hash:
+            new_session = await Session.create_session(user)
+            response = await make_response(render_template("index.html", message="Logado com sucesso!"))
+            response.set_cookie("current_session", new_session.session_id)
+            return response
+
+      return await render_template("login.html", message="Credenciais incorretas.")
+   else:
+      session = await Session.find({"session_id": request.cookies.get("current_session")}).first_or_none()
+      if session is not None:
+         if not (await session.is_expired()):
+            await session.renew()
+            return await render_template("index.html", message="Você já está logado!")
+
+      return await render_template("login.html")
 
 app.run()
