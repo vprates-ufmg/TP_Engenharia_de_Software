1bfb59d684c762c74c63d090391d2dd6f53e81fa
matheusflavio <flavio.matheusgsilva@gmail.com>
2023-05-04 15:10:28 -0300

    Adicionanado alguns comentários

--- src/static/js/change-visibility.js
+++ src/static/js/change-visibility.js
@@ -1,10 +1,11 @@
+// Função responsável por alterar a visibilidade da senha.
 const togglePassword = document.querySelector('#toggle-password');
   const password = document.querySelector('#password');
 
   togglePassword.addEventListener('click', function (e) {
-    // toggle the type attribute
+    // Altera o tipo de input. Possibilita tornar visível/invisível a senha
     const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
     password.setAttribute('type', type);
-    // toggle the eye slash icon
+    // Altera o ícone com o olho/olho cortado
     this.classList.toggle('fa-eye-slash');
 });
--- src/static/js/hash.js
+++ src/static/js/hash.js
@@ -2,7 +2,7 @@ const form = document.getElementById('form');
 const passwordInput = document.getElementById('password');
 const passwordHashInput = document.getElementById('password-hash');
 
-
+// Função responsável por gerar o hash da senha (autoral enquanto não temos a integração de react + flask feita)
 async function generatePasswordHash(password) {
     const encoder = new TextEncoder();
     const data = encoder.encode(password);
@@ -12,7 +12,6 @@ async function generatePasswordHash(password) {
     return hashHex;
   }
   
-
 form.addEventListener('submit', async (event) => {
   const password = passwordInput.value;
   const passwordHash = await generatePasswordHash(password);
