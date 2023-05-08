13177b43fa7aa08cd1e3ac456345d9ed48c53a8a
matheusflavio <flavio.matheusgsilva@gmail.com>
2023-05-08 11:53:03 -0300

    Revomendo código inútil e alterando algumas coisas

--- frontend/review-profs/src/App.js
+++ frontend/review-profs/src/App.js
@@ -5,18 +5,13 @@ import RegisterIcon from "./Images/register-icon.png";
 import LoginIcon from "./Images/login-icon.png"
 
 import Form from "./Pages/Forms/Form"
-import CaixaDisciplinas from "./Components/CaixaDisciplinas"
-import CaixaProfessores from "./Components/CaixaProfessores"
-import CaixaSemestres from "./Components/CaixaSemestres"
-import FetchReview from "./Components/FetchReview"
-import CreateReview from "./Components/CreateReview"
 import Home from "./Pages/Home"
 import LandingPage from "./Pages/LandingPage"
 function App() {
   return (
     <Router>
       <Routes>
-      <Route exact path="/landingpage" element={<LandingPage/>} />
+        <Route exact path="/landingpage" element={<LandingPage />} />
         <Route exact path='/' element={< Form
           pageType='Login'
           url='http://127.0.0.1:5000/login'
@@ -28,7 +23,7 @@ function App() {
           buttonText="Login"
           pText="Ainda não possui conta? "
           aText="Cadastre-se"
-          />} />
+        />} />
         <Route exact path='/login' element={< Form
           pageType='Login'
           url='http://127.0.0.1:5000/login'
@@ -40,7 +35,7 @@ function App() {
           buttonText="Login"
           pText="Ainda não possui conta? "
           aText="Cadastre-se"
-          />} />
+        />} />
         <Route exact path='/register' element={< Form
           pageType='Registro'
           url='http://127.0.0.1:5000/register'
@@ -52,14 +47,9 @@ function App() {
           buttonText="Cadastre-se"
           pText="Já possui conta? "
           aText="Faça Login"
-          />} />
-          <Route exact path='/disciplinas' element={<CaixaDisciplinas></CaixaDisciplinas>}></Route>
-          <Route exact path='/professores' element={<CaixaProfessores></CaixaProfessores>}></Route>
-          <Route exact path='/semestres' element={<CaixaSemestres></CaixaSemestres>}></Route>
-          <Route exact path='/home' element={<Home></Home>}></Route>
-          <Route exact path='/feed' element={<Home></Home>}></Route>
-          <Route exact path='/create_review' element={<CreateReview></CreateReview>}></Route>
-          <Route exact path='/fetch_review' element={<FetchReview></FetchReview>}></Route>
+        />} />
+        <Route exact path='/home' element={<Home></Home>}></Route>
+        <Route exact path='/feed' element={<Home></Home>}></Route>
       </Routes>
     </Router>
   )
